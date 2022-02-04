a7a72b6890bdc5e71744dac592075c365bf77457
ParasJain-ui <parasjain1237@gmail.com>
2022-02-04 21:41:28 +0530

    donegit add .

--- clock.py
+++ clock.py
@@ -31,7 +31,7 @@ def helper(r, i):
     return count
 
 #schedule 1
-@scheduler.scheduled_job('interval',minutes=1)
+@scheduler.scheduled_job('interval',minutes=60)
 def update_sheet():
     print ("hello")
     for i in range(len(data["Name"])):
