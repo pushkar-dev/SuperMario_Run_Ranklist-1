f384158b439afa857f3275b780c8cf62464c3006
ParasJain-ui <parasjain1237@gmail.com>
2022-02-04 22:48:37 +0530

    donegit add .

--- clock.py
+++ clock.py
@@ -14,24 +14,27 @@ data = pandas.read_csv("handles")
 
 # counting the number of Questions for a particular user
 def helper(r, i):
-    count = 0
-    json_data = r. json()
     try:
-        for j in range(len(json_data["result"])):
-            try:
-                if (json_data["result"][j]["verdict"] == "OK" and 
-                    json_data["result"][j]["problem"]["rating"] >= max (data['ratings'][i], 1200) and
-                    json_data["result"][j]["creationTimeSeconds"]>=TIME_STAMP and
-                    json_data["result"][j]["author"]["ghost"]==False):
-                    count += 1
-            except:
-                pass
+        count = 0
+        json_data = r. json()
+        try:
+            for j in range(len(json_data["result"])):
+                try:
+                    if (json_data["result"][j]["verdict"] == "OK" and 
+                        json_data["result"][j]["problem"]["rating"] >= max (data['ratings'][i], 1200) and
+                        json_data["result"][j]["creationTimeSeconds"]>=TIME_STAMP and
+                        json_data["result"][j]["author"]["ghost"]==False):
+                        count += 1
+                except:
+                    pass
+        except:
+            pass
     except:
-        pass
+        count = 0 
     return count
 
 #schedule 1
-@scheduler.scheduled_job('interval',minutes=60)
+@scheduler.scheduled_job('interval',minutes = 1)
 def update_sheet():
     print ("hello")
     for i in range(len(data["Name"])):
