21ef93f92c78fd09c772f4a2825d56b9b3c92cdd
ParasJain-ui <parasjain1237@gmail.com>
2022-02-04 21:37:01 +0530

    its done

--- clock.py
+++ clock.py
@@ -14,14 +14,14 @@ data = pandas.read_csv("handles")
 
 # counting the number of Questions for a particular user
 def helper(r, i):
-    count = 20
+    count = 0
     json_data = r. json()
     try:
         for j in range(len(json_data["result"])):
             try:
                 if (json_data["result"][j]["verdict"] == "OK" and 
                     json_data["result"][j]["problem"]["rating"] >= max (data['ratings'][i], 1200) and
-                    json_data["result"][j]["problem"]["startTimeSeconds"]>=TIME_STAMP and
+                    json_data["result"][j]["creationTimeSeconds"]>=TIME_STAMP and
                     json_data["result"][j]["author"]["ghost"]==False):
                     count += 1
             except:
